cf8ac05090f341ad69e46a003af62e942f0ca3a5
Ege <hsgngr@gmail.com>
2020-02-12 14:53:10 +0000

    Kernel Density Estimation

---
+++ PythonExercises/ExerciseEnvironment/Hey.py
@@ -0,0 +1,5 @@
+"""
+Created by  Ege of the House Hoşgüngör, the First of His Name  at 14:51 11.02.2020
+"""
+from sklearn.grid_search import GridSearchCV
+from sklearn.cross_validation import LeaveOneOut
